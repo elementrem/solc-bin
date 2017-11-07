8338d0bdc3335cb1475abf211cfe78153e791c2d
elementrem <elementrem@gmail.com>
2017-11-08 06:56:39 +0900

    update ele-solc 0.4.18

--- bin/list.js
+++ bin/list.js
@@ -1,7 +1,7 @@
 
 var soljsonSources = [
   "soljson-v0.4.10+commit-610c4b0.js",
-  "soljson-v0.4.18+commit.01dcd70"
+  "soljson-v0.4.18+commit.01dcd70.js"
 ];
 var soljsonReleases = {
   "0.4.10": "soljson-v0.4.10+commit-610c4b0.js",
